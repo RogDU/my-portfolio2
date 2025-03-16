e347ba157b72cf7bc7f42d5804fa6795ed7577ad
Rogério Duarte Xavier <rogdu2@gmail.com>
2025-03-16 11:49:26 -0300

    feat: Mudanças em arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import Navbar from "./components/Navbar/Navbar"
 import ProjectsSection from "./Pages/Home/sections/ProjectsSection/ProjectsSection"
 import Footer from "./components/Footer/Footer"
 
-const App: React.FC = () => {
 
+const App: React.FC = () => {
+ 
   return (
     <>
       <Navbar />
